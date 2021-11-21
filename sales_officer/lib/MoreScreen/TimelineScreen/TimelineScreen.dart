50fd19b3c7df9b88428ec300d48e9f0f1a61b798
sjtsh <74091619+sjtsh@users.noreply.github.com>
2021-11-21 10:30:48 +0545

    conflict merge in timeline

--- sales_officer/lib/MoreScreen/TimelineScreen/TimelineScreen.dart
+++ sales_officer/lib/MoreScreen/TimelineScreen/TimelineScreen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:timelines/timelines.dart';
-
 class TimelineScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
